a9fed24c3c88e203f5218076b379e52288178242
guillegalor <guillegalor@gmail.com>
2020-03-27 11:45:20 +0100

    Añade ejercicio 2 a)-c)

--- P1/src/ej2.py
+++ P1/src/ej2.py
@@ -1,17 +1,33 @@
 # General imports
 import numpy as np
-import matplotlib.pyplot as plt
 import random
+
+# Matplotlib imports
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
 
 # Set seed for random
 random.seed(1)
 
+#----------------------------------------#
+#---------- Auxiliar Functions ----------#
+#----------------------------------------#
+
+def to_numpy(func):
+    """
+    Decorator function to convert functions
+    to Numpy format
+    """
+
+    def numpy_func(w):
+        return func(*w)
+
+    return numpy_func
+
 def wait():
     input("\n--- Pulsar tecla para continuar ---\n")
 
-#----------------------------------------#
-#------------ Ejercicio 2 ---------------#
-#----------------------------------------#
+#------------ Ejercicio 1 ---------------#
 
 def readData(file_x, file_y):
     # Load files with data
@@ -28,7 +44,7 @@ def readData(file_x, file_y):
                 y.append(1)
             else:
                 y.append(-1)
-        x.append(np.array([1, datax[i][0], datax[i][1]]))
+            x.append(np.array([1, datax[i][0], datax[i][1]]))
 
     x = np.array(x, np.float64)
     y = np.array(y, np.float64)
@@ -44,7 +60,6 @@ def Err(x,y,w):
 def dErr(x, y, w):
     return 2/len(x) * (x.T.dot(x.dot(w) - y))
 
-# Stochastic gradiente descent
 def sgd(x, y, lr=0.01, max_iters=1000, batch_size=32):
     """
     Stochastic gradient descent applied to linear regression
@@ -70,25 +85,88 @@ def sgd(x, y, lr=0.01, max_iters=1000, batch_size=32):
         w = w - lr * dErr(x[batch_idxs], y[batch_idxs], w)
 
         # Updates iterations and batch
-        iterations += 1
+        iters += 1
         batch_start += batch_size
         if batch_start >= len(x):
             batch_start = 0
 
     return w
 
+def pseudoinverse(x, y):
+    """
+    Pseudoinverse algorithm applied to linear regression
+    - x              : Data (array of arrays with features of each element)
+    - y              : Labels for each element
+    """
+
+    # Gets singular value decomposition
+    u, s, v = np.linalg.svd(x)
+    # Compute the inverse of the matrix D (only use the diagonal part of the matrix d)
+    # avoiding 0 values (or really close to 0)
+    d = np.diag([0 if np.allclose(p, 0) else 1/p for p in s])
+    # Compute the pseudoinverse of X
+    pseudoinverse = v.T.dot(d).dot(d).dot(v).dot(x.T)
+    return pseudoinverse.dot(y)
+
 # Data reading
 x, y = readData('../data/X_train.npy', '../data/y_train.npy')
 # Test data reading
 x_test, y_test = readData('../data/X_test.npy', '../data/y_test.npy')
+print(x.shape, y.shape)
 
 print ('EJERCICIO SOBRE REGRESION LINEAL\n')
 print ('Ejercicio 1\n')
-# Gradiente descendente estocastico
-w = sgd(x, y)
 
-print ('Bondad del resultado para grad. descendente estocastico:\n')
+# Gradiente descendente estocástico
+w = sgd(x, y)
+print ('Bondad del resultado para grad. descendente estocástico:\n')
 print ("Ein: ", Err(x, y, w))
 print ("Eout: ", Err(x_test, y_test, w))
 
+w = pseudoinverse(x, y)
+print('\nBondad del resultado para pseudo-inversa:')
+print("  Ein:  ", Err(x, y, w))
+print("  Eout: ", Err(x_test, y_test, w))
+
 wait()
+
+#------------ Ejercicio 2 ---------------#
+
+# Simula datos en un cuadrado [-size,size]x[-size,size]
+def simula_unif(N, d, size):
+    return np.random.uniform(-size, size, (N, d))
+
+# EXPERIMENTO
+# a) Muestra de entrenamiento N = 1000, cuadrado [-1,1]x[-1,1]
+
+print ('Ejercicio 2\n')
+print ('Muestra N = 1000, cuadrado [-1,1]x[-1,1]')
+
+X = simula_unif(1000, 2, 1)
+plt.plot([a[0] for a in X], [a[1] for a in X], 'bo')
+plt.show()
+
+# b) Mapa de etiquetas
+@to_numpy
+def f(x1, x2):
+    return np.sign((x1 - 0.2)**2 + x2**2 - 0.6)
+
+# Compute the labels of 90% of elements
+y = f(X[:900].T)
+# Generate the remaining 10% randomly
+y_random = np.random.choice([-1, 1], 100)
+# Join everything
+y = np.hstack((y, y_random))
+
+# Generates the label map and
+scatter = plt.scatter(X[:, 0], X[:, 1], c=y, cmap=ListedColormap(['r', 'g']))
+plt.show()
+
+# c) Ajustar un modelo de regresión lineal utilizando el gradiente descendiente estocástico
+X = np.array([[1, x[0], x[1]] for x in X])
+
+w = sgd(X,y)
+print ('Bondad del resultado en el experimento para grad. descendente estocástico:\n')
+print ("Ein: ", Err(X, y, w))
+
+# Ejecutar el experimento a)-c) 1000 veces
